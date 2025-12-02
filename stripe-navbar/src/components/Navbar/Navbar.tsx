18f0f0718094ec931a4262e091123b4fda5c7cb2
harshitshesh <harshitshesh78@gmail.com>
2025-12-02 17:15:50 +0530

    assignment compeleted

--- stripe-navbar/src/components/Navbar/Navbar.tsx
+++ stripe-navbar/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import  { useEffect, useRef, useState } from "react";
 import { Menu, X, ChevronDown } from "lucide-react";
 import { navbarItems } from "../../data/Navbaritems";
 import MegaMenu from "./Megamenu";
